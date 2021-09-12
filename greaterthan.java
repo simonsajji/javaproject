518d4006e24a6b374a85f661df8eac8a35d56af3
simonsajji <85159792+simonsajji@users.noreply.github.com>
2021-09-12 17:01:49 +0530

    Add files via upload

---
+++ greaterthan.java
@@ -0,0 +1,19 @@
+package javanew;
+
+public class greaterthan {
+	public static void main(String[] args) {
+		int x=100;
+		int y=40;
+		int z=30;
+		if (x>y && x>z){
+			System.out.println("X is the greatest");
+		}
+		else if(y>x && y>z){
+			System.out.println("y is the greatest");
+		}
+		else{
+			System.out.println("z is the greatest");
+		}
+			
+}
+}
